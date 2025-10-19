c069dd2e914daf925f6cf53d3a3ad09d34e66791
Pranav2325 <pranavdhondkar@gmail.com>
2025-10-19 20:33:07 +0530

    create navbar,banner and hero section for home page

---
+++ client/src/pages/ResumeBuilder.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useParams } from "react-router-dom";
+
+const ResumeBuilder = () => {
+    
+    
+  return (
+    
+    <div>
+        <h1>ResumeBuilder page</h1>
+    </div>
+  )
+}
+
+export default ResumeBuilder
