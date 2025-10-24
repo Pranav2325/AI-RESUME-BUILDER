a0c563d147901ccd537695824bbc739610019aa9
Pranav2325 <pranavdhondkar@gmail.com>
2025-10-24 20:28:54 +0530

    create section navigation and progress bar

---
+++ client/src/components/PersonalInforForm.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const PersonalInforForm = ({
+  data,
+  onChange,
+  removeBackground,
+  setRemoveBackground,
+}) => {
+  return (
+    <div>
+      <h3 className="text-lg font-semibold text-gray-900">
+        Personal Information
+      </h3>
+      <p className="text-sm text-gray-600">
+        Get Started with the personal information
+      </p>
+      <div className="flex ">
+
+      </div>
+    </div>
+  );
+};
+
+export default PersonalInforForm;
